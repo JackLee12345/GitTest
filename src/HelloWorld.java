cf59fc021eea7df7794a508cc9852e4839df777a
liyou <1014041610@qq.com>
2019-11-21 16:29:48 +0800

    888

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("8888");
-        System.out.println("2222");
-        System.out.println("3333");
-        System.out.println("44");
     }
 }
