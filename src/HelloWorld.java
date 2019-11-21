00466ebeacb8b763bae8c6f16fd6148a9268f97c
liyou <1014041610@qq.com>
2019-11-21 16:34:24 +0800

    000

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("8888");
         System.out.println("9999");
-        System.out.println("000");
+        System.out.println("00");
     }
 }
